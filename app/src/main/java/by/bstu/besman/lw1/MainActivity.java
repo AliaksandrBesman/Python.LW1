345fa809709119ee407fe85ab8c74931f6130f02
Aliaksandr Besman <aliaksandr.besman001@gmail.com>
2021-09-04 09:13:29 +0300

    Initial commit

---
+++ app/src/main/java/by/bstu/besman/lw1/MainActivity.java
@@ -0,0 +1,14 @@
+package by.bstu.besman.lw1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//TODO Add New Method
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
